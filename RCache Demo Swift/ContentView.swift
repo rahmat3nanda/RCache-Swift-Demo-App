0b743cee175150bd945d884f934e7a80c1918acb
Rahmat Trinanda <rahmat3nanda@gmail.com>
2024-08-26 16:45:50 +0700

    Initial Commit

---
+++ RCache Demo Swift/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RCache Demo Swift
+//
+//  Created by Rahmat Trinanda Pramudya Amar on 26/08/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RCache Demo Swift/RCache_Demo_SwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  RCache_Demo_SwiftApp.swift
+//  RCache Demo Swift
+//
+//  Created by Rahmat Trinanda Pramudya Amar on 26/08/24.
+//
+
+import SwiftUI
+
+@main
+struct RCache_Demo_SwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
