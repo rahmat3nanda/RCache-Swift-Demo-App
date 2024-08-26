04ff63de4d43a02c64034171c6a13b14fb868b19
Rahmat Trinanda <rahmat3nanda@gmail.com>
2024-08-26 18:46:57 +0700

    Update Home View

--- RCacheDemoSwift/HomeView.swift
+++ RCacheDemoSwift/HomeView.swift
@@ -8,14 +8,33 @@
 import SwiftUI
 
 struct HomeView: View {
+    
+    @EnvironmentObject var router: Router
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            VStack(content: {
+                Text("Placeholder")
+            })
+            FloatingMenuView {
+                
+                Button(action: {
+                    
+                }) {
+                    VStack {
+                        Image(systemName: "key.fill")
+                            .frame(minWidth: 24)
+                            .scaledToFill()
+                            .padding()
+                            .background(Color.blue)
+                            .foregroundColor(.white)
+                            .clipShape(Circle())
+                            .shadow(radius: 5)
+                        Text("Key")
+                    }
+                }
+            }
         }
-        .padding()
     }
 }
 
