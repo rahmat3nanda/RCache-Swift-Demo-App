a189a27eddbddbac5e492e7694cd4369fd3e8f4b
Rahmat Trinanda <rahmat3nanda@gmail.com>
2024-08-26 18:52:18 +0700

    Create KeyView

---
+++ RCacheDemoSwift/Views/KeyView.swift
@@ -0,0 +1,20 @@
+//
+//  KeyView.swift
+//  RCacheDemoSwift
+//
+//  Created by Rahmat Trinanda Pramudya Amar on 26/08/24.
+//
+
+import SwiftUI
+
+struct KeyView: View {
+    var body: some View {
+        VStack {
+            
+        }
+    }
+}
+
+#Preview {
+    KeyView()
+}
