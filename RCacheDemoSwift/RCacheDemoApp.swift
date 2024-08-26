2443638833be1774765b119c41242c3576a115f1
Rahmat Trinanda <rahmat3nanda@gmail.com>
2024-08-26 16:54:29 +0700

    Update path

--- RCacheDemoSwift/RCacheDemoApp.swift
+++ RCacheDemoSwift/RCacheDemoApp.swift
@@ -1,5 +1,5 @@
 //
-//  RCache_Demo_SwiftApp.swift
+//  RCacheDemoApp.swift
 //  RCache Demo Swift
 //
 //  Created by Rahmat Trinanda Pramudya Amar on 26/08/24.
@@ -8,7 +8,7 @@
 import SwiftUI
 
 @main
-struct RCache_Demo_SwiftApp: App {
+struct RCacheDemoApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
