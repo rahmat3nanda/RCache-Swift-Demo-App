3bc9a6913846360c1d566f830cf0eecc4349ccc0
Rahmat Trinanda <rahmat3nanda@gmail.com>
2024-08-26 19:13:51 +0700

    Create RCache+Key.swift

---
+++ RCacheDemoSwift/RCache+Key.swift
@@ -0,0 +1,12 @@
+//
+//  RCache+Key.swift
+//  RCacheDemoSwift
+//
+//  Created by Rahmat Trinanda Pramudya Amar on 26/08/24.
+//
+
+import RCache
+
+extension RCache.Key {
+    static let savedKeys = RCache.Key("savedKeys")
+}
