04670771d923aba5e41b05fc2914c906af2c2650
Rahmat Trinanda <rahmat3nanda@gmail.com>
2024-08-27 13:21:50 +0700

    update view

--- RCacheDemoSwift/Views/HomeView.swift
+++ RCacheDemoSwift/Views/HomeView.swift
@@ -18,26 +18,49 @@ struct HomeView: View {
                 Text("RCache")
             })
             FloatingMenuView(isMenuOpen: $isMenuOpen) {
-                Button(action: {
+                ItemMenuView(imageName: "key.fill", title: "Key") {
                     isMenuOpen = false
                     router.navigate(to: .key)
-                }) {
-                    VStack {
-                        Image(systemName: "key.fill")
-                            .frame(minWidth: 24)
-                            .scaledToFill()
-                            .padding()
-                            .background(Color.blue)
-                            .foregroundColor(.white)
-                            .clipShape(Circle())
-                            .shadow(radius: 5)
-                        Text("Key")
-                    }
                 }
             }
         }
         .navigationBarTitle("RCache")
         .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+fileprivate struct ItemMenuView: View {
+    let imageName: String
+    let title: String
+    let action: () -> Void
+    
+    var body: some View {
+        Button(action: action) {
+            VStack {
+                Image(systemName: imageName)
+                    .frame(minWidth: 24)
+                    .scaledToFill()
+                    .padding()
+                    .background(Color.blue)
+                    .foregroundColor(.white)
+                    .clipShape(Circle())
+                    .shadow(radius: 5)
+                VStack {
+                    Text(title)
+                        .tint(.white)
+                        .padding(
+                            .init(
+                                top: 4,
+                                leading: 12,
+                                bottom: 4,
+                                trailing: 12
+                            )
+                        ).frame(minWidth: 36)
+                }
+                .background(.blue)
+                .clipShape(.rect(cornerRadius: 6))
+            }
+        }
     }
 }
 
