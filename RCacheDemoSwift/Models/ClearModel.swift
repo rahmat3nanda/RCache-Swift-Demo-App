2ee570b5b35d39627cfd7607310a99cfc8f4e316
Rahmat Trinanda <rahmat3nanda@gmail.com>
2024-08-31 21:02:54 +0700

    Create Clear Model

---
+++ RCacheDemoSwift/Models/ClearModel.swift
@@ -0,0 +1,14 @@
+//
+//  ClearModel.swift
+//  RCacheDemoSwift
+//
+//  Created by Rahmat Trinanda Pramudya Amar on 31/08/24.
+//
+
+import Foundation
+
+enum ClearType: String {
+    case common = "All General Data"
+    case credentials = "All Credentials Data"
+    case all = "All Data"
+}
